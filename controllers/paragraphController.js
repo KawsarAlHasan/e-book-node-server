e2bb662b41ef432d538373df0a67b89a7ed0e4a4
KawsarAlHasan <kawsaralhasan.420@gmail.com>
2024-10-23 18:16:03 +0600

    upadate mark text

--- controllers/paragraphController.js
+++ controllers/paragraphController.js
@@ -205,3 +205,169 @@ exports.deleteParagraph = async (req, res) => {
     });
   }
 };
+
+// add MarkText
+exports.addMarkText = async (req, res) => {
+  try {
+    const { para_id, text, definition } = req.body;
+
+    if (!para_id || !text || !definition) {
+      return res.status(400).send({
+        success: false,
+        message: "Please provide para_id, text & definition field",
+      });
+    }
+
+    // Insert Mark Text into the database
+    const [result] = await db.query(
+      "INSERT INTO mark_text (para_id, text, definition) VALUES (?, ?, ?)",
+      [para_id, text, definition]
+    );
+
+    // Check if the insertion was successful
+    if (result.affectedRows === 0) {
+      return res.status(500).send({
+        success: false,
+        message: "Failed to insert Mark Text, please try again",
+      });
+    }
+
+    // Send success response
+    res.status(200).send({
+      success: true,
+      message: "Mark Text inserted successfully",
+    });
+  } catch (error) {
+    res.status(500).send({
+      success: false,
+      message: "An error occurred while inserting the Mark Text",
+      error: error.message,
+    });
+  }
+};
+
+// update MarkText
+exports.updateMarkText = async (req, res) => {
+  try {
+    const mark_id = req.params.id;
+
+    const { text, definition } = req.body;
+
+    // Check if MarkText exists
+    const [existingMarkText] = await db.query(
+      "SELECT * FROM mark_text WHERE id = ?",
+      [mark_id]
+    );
+
+    if (!existingMarkText || existingMarkText.length === 0) {
+      return res.status(404).send({
+        success: false,
+        message: "MarkText not found",
+      });
+    }
+
+    // Execute the update query
+    const [result] = await db.query(
+      "UPDATE mark_text SET text = ?, definition= ? WHERE id = ?",
+      [
+        text || existingMarkText[0].text,
+        definition || existingMarkText[0].definition,
+        mark_id,
+      ]
+    );
+
+    // Check if the MarkText was updated successfully
+    if (result.affectedRows === 0) {
+      return res.status(404).send({
+        success: false,
+        message: "MarkText not found or no changes made",
+      });
+    }
+
+    // Success response
+    res.status(200).send({
+      success: true,
+      message: "MarkText updated successfully",
+    });
+  } catch (error) {
+    // Handle errors
+    res.status(500).send({
+      success: false,
+      message: "Error updating MarkText",
+      error: error.message,
+    });
+  }
+};
+
+// delete MarkText
+exports.deleteMarkText = async (req, res) => {
+  try {
+    const mark_id = req.params.id;
+
+    // Check if the mark_text exists in the database
+    const [SubTOC] = await db.query(`SELECT * FROM mark_text WHERE id = ?`, [
+      mark_id,
+    ]);
+
+    // If mark_text not found, return 404
+    if (SubTOC.length === 0) {
+      return res.status(404).send({
+        success: false,
+        message: "mark_text not found",
+      });
+    }
+
+    // Proceed to delete the SubTOC
+    await db.query(`DELETE FROM favorite_mark_text WHERE mark_text_id = ?`, [
+      mark_id,
+    ]);
+
+    // Proceed to delete the SubTOC
+    const [result] = await db.query(`DELETE FROM mark_text WHERE id = ?`, [
+      mark_id,
+    ]);
+
+    // Check if deletion was successful
+    if (result.affectedRows === 0) {
+      return res.status(500).send({
+        success: false,
+        message: "Failed to delete mark_text",
+      });
+    }
+
+    // Send success response
+    res.status(200).send({
+      success: true,
+      message: "mark_text deleted successfully",
+    });
+  } catch (error) {
+    res.status(500).send({
+      success: false,
+      message: "Error deleting mark_text",
+      error: error.message,
+    });
+  }
+};
+
+// get Mark text
+exports.getMarkText = async (req, res) => {
+  try {
+    const para_id = req.params.id;
+
+    const [data] = await db.query("SELECT * FROM mark_text WHERE para_id =?", [
+      para_id,
+    ]);
+
+    res.status(200).send({
+      success: true,
+      message: "Get Single Mark Text",
+      data: data,
+    });
+  } catch (error) {
+    res.status(500).send({
+      success: false,
+      message: "Error in Get Mark Text",
+      error: error.message,
+    });
+  }
+};
--- routes/paragraphRoute.js
+++ routes/paragraphRoute.js
@@ -4,6 +4,10 @@ const {
   getSingleParagraph,
   updateParagraph,
   deleteParagraph,
+  addMarkText,
+  updateMarkText,
+  deleteMarkText,
+  getMarkText,
 } = require("../controllers/paragraphController");
 
 const router = express.Router();
@@ -12,5 +16,10 @@ router.post("/create", createParagraph);
 router.get("/:id", getSingleParagraph);
 router.put("/update/:id", updateParagraph);
 router.delete("/delete/:id", deleteParagraph);
+
+router.post("/marktext/add", addMarkText);
+router.put("/marktext/update/:id", updateMarkText);
+router.delete("/marktext/delete/:id", deleteMarkText);
+router.get("/marktext/:id", getMarkText);
 
 module.exports = router;
